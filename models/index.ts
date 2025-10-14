00487b7cd2396cbb90db4acaacbc6b1f5b00ed99
Amadou-dot <amadou.sseck@gmail.com>
2025-10-13 21:40:47 -0700

    Remove Customer model and clean up exports

--- models/index.ts
+++ models/index.ts
@@ -1,6 +1,5 @@
 // Export all models
 export { default as Cabin, type ICabin } from "./Cabin";
-export { default as Customer, type ICustomer } from "./Customer";
 export { default as Booking, type IBooking } from "./Booking";
 export { default as Settings, type ISettings } from "./Settings";
 export { Experience, type IExperience } from "./Experience";
